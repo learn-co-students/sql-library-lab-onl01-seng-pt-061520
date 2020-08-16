ed984db8166de201ceb7ee5adf4b2decb8eabd7d
drewsview34 <drewsview34@outlook.com>
2020-08-15 20:07:16 -0700

    Created the sql method that SELECT the series author and subgenres

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,6 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year
+
    FROM books
    JOIN series
    ON books.series_id = series.id
@@ -9,6 +10,7 @@ end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT name, motto
+
    FROM characters
    ORDER BY
    LENGTH(motto)
@@ -18,11 +20,23 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  ""
+  "SELECT species,
+   COUNT(species) AS prolific
+
+   FROM characters
+   GROUP BY species
+   ORDER BY prolific
+   DESC
+   LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name
+   FROM series
+   JOIN authors
+   ON series.author_id = authors.id
+   JOIN subgenres
+   ON series.subgenre_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
