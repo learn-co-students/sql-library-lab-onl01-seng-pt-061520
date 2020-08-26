7941e66ae6f19f100c3f781d4d645b5fb1da90d1
Cameron Hudson <camnn3@yahoo.com>
2020-08-26 19:35:56 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,7 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors
   JOIN series ON series.author_id = authors.id
-  JOIN subgenres ON series.subgenre_id = subgenres.id
+  JOIN subgenres ON series.subgenre_id = subgenres.id;
   "
 end
 
@@ -35,7 +35,8 @@ def select_series_title_with_most_human_characters
   JOIN character_books ON character_books.book_id = books.id
   JOIN characters ON character_books.characters_id = characters.id
   WHERE characters.species = 'human'
-  GROUP BY series.title"
+  GROUP BY series.title
+  ORDER BY COUNT (*) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
