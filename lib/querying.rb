a1bef3cb31d4abea74d07476a3fcdd9b22a533cc
Cameron Hudson <camnn3@yahoo.com>
2020-08-26 19:24:33 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,7 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors
-  JOIN series ON authors.series_id = series.id
+  JOIN series ON series.authors_id = authors.id
   
   "
 end
