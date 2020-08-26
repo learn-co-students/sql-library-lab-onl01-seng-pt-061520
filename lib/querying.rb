6f39d81685d5df3983156b3ca306885d045ba946
Cameron Hudson <camnn3@yahoo.com>
2020-08-26 19:32:04 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,7 +33,7 @@ def select_series_title_with_most_human_characters
   FROM series
   JOIN books ON books.series_id = series.id
   JOIN character_books ON character_books.book_id = books.id
-  JOIN "
+  JOIN characters ON character"
 end
 
 def select_character_names_and_number_of_books_they_are_in
