9675a6777d35f3b861fbe33d888f900d46c492c8
Cameron Hudson <camnn3@yahoo.com>
2020-08-26 19:41:05 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -41,6 +41,7 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT (*) as book_count from character_books
-  JOIN characters ON characters.character_book
+  JOIN characters ON character_books.character_id = characters.id
+  
   "
 end
