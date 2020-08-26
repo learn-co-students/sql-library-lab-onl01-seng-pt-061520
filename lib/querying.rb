384b0cc4508cf88db503d6b1e7e22b5e264cd6c5
Cameron Hudson <camnn3@yahoo.com>
2020-08-26 19:14:47 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT (*) characters"
+  "SELECT species, COUNT (*) 
+  FROM characters
+  "
 end
 
 def select_name_and_series_subgenres_of_authors
