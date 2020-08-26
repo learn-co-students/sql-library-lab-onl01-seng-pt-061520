d68716fc459ad26e666f492588729c73639ca77d
Cameron Hudson <camnn3@yahoo.com>
2020-08-26 19:39:01 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -40,6 +40,6 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT (*) as
+  "SELECT characters.name, COUNT (*) as book_count
   "
 end
